f3dec7f6b872a9b39b2119d299e1e0a03919eb86
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-11-10 15:59:07 -0500

    Added teensy interface to the esp8266.

---
+++ teensy_esp8266.h
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <string.h>
+//Wifi Defines
+#define AT_RESET        "AT+RST\r\n"
+#define AT_ACCEPT_CONNS "AT+CWMODE=3\r\n"
+#define AT_STATION      "AT+CWMODE=1\r\n"
+#define AT_FIRMWARE     "AT+GMR\r\n"
+#define AT_SCAN         "AT+CWLAP\r\n"
+#define AT_GET_IP       "AT+CIFSR\r\n"
+#define AT_CREAT_SERVER "AT+CIPSERVER=1,5555\r\n"
+#define AT_MULTI_CONN   "AT+CIPMUX=1\r\n"
+
+inline void sendCommand(HardwareSerial *hwserial, char *command) {
+  (*hwserial).write((uint8_t *)command, strlen(command));
+}
