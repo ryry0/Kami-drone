aec58df55ceb54890c11f5e455d700a1142a359e
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-11-25 13:32:22 -0500

    Added kami-drone.h. This has state information in it.

---
+++ Kami-drone.h
@@ -0,0 +1,12 @@
+#ifndef KAMI_DRONE_H_
+#include "teensy_accel.h"
+#include "teensy_gyro.h"
+#include "packet.h"
+
+typedef enum kami_drone_state_e {
+  STATE_LANDED,
+  STATE_FLYING,
+  STATE_HARD_KILL,
+} kami_drone_state_t;
+
+#endif KAMI_DRONE_H_
