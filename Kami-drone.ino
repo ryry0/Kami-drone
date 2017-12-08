1fc54099be9db42081d22d9b357e0fc2ec70b1b0
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-12-08 00:59:13 -0500

    Rate control implementation.

--- Kami-drone.ino
+++ Kami-drone.ino
@@ -303,12 +303,14 @@ void controlLoop() {
   //run pid
   if (kami_drone.state != STATE_LANDED) {
     //ROLL AXIS
-    float roll_vel_command = pid_FeedbackCtrl(
+    float roll_vel_command = kami_drone.roll_commanded;
+    /*pid_FeedbackCtrl(
         (pid_data_t *) &kami_drone.roll_pid,
         kami_drone.roll_commanded,
         kami_drone.roll_shift,
         DELTA_TIME,
         pid_velocUpdate);
+    */
 
     kami_drone.motor2_correction = -0.5*pid_FeedbackCtrl(
         (pid_data_t *) &kami_drone.roll_vel_pid,
@@ -320,12 +322,14 @@ void controlLoop() {
     kami_drone.motor4_correction = -kami_drone.motor2_correction;
 
     //PITCH AXIS
-    float pitch_vel_command = pid_FeedbackCtrl(
+    float pitch_vel_command = kami_drone.pitch_commanded;
+    /*pid_FeedbackCtrl(
         (pid_data_t *) &kami_drone.pitch_pid,
         kami_drone.pitch_commanded,
         kami_drone.pitch_shift,
         DELTA_TIME,
         pid_velocUpdate);
+    */
 
     kami_drone.motor1_correction = -0.5*pid_FeedbackCtrl(
         (pid_data_t *) &kami_drone.pitch_vel_pid,
